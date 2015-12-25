abad2a67bf49b21cfe4eef9f3ada9b4b4699a2e1
Ashish Mishra <ashish@gooru.org>
2015-12-25 12:17:02 +0530

    Initial repo creation

---
+++ src/main/java/org/gooru/nucleus/handlers/taxonomy/bootstrap/TaxonomyVerticle.java
@@ -0,0 +1,9 @@
+package org.gooru.nucleus.handlers.taxonomy.bootstrap;
+
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * Created by ashish on 25/12/15.
+ */
+public class TaxonomyVerticle extends AbstractVerticle {
+}
